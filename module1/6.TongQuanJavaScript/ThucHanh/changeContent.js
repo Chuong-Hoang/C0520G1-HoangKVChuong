5093734e632fe171922c46f14743db2fd3b8b3f8
Chuong-Hoang <hoangkvchuong@gmail.com>
2020-06-02 15:13:51 +0700

    update bai thuc hanh JavaScript

---
+++ module1/6.TongQuanJavaScript/ThucHanh/changeContent.js
@@ -0,0 +1,7 @@
+function changeContent() {
+    document.getElementById('demo').innerHTML=
+        "<p><mark>Bạn vừa <b>Click</b> vào nút Button nên thông tin vừa được thay đổi:</mark><br><br>" +
+        "<img src='Chuong.jpg' alt='Ảnh của Chương' style='height:150px;width:150px;border-radius: 50%;border:2px ridge #555555;'><br><br>"
+        + "<iframe width ='460' height='250' src='https://www.youtube.com/embed/WU9mhiJ4wgU\' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></p>";
+
+}
